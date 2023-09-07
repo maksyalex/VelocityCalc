62a98d8bcfd57d60e0c04c663cc4ab3315283309
Alex <maksyalex@gmail.com>
2023-09-07 12:47:54 +1000

    first commit

---
+++ velocityCalculator.py
@@ -0,0 +1,12 @@
+#Behold!...... a basic velocity calculator
+#v = d/t
+import math 
+# d is distance... 
+d = float(input("Enter distance you are going / have gone just numbers as a whole or decimal in KM: "))
+# t is time...
+t = float(input("How many minutes did it take you: "))
+# v is velocity 
+v = d/t 
+print('Your velocity is ', v, "per minute")
+# there is much more to it, this is just extremely basic calculation based on some very traditional physics concepts. 
+print("********** MORE TO BE ADDED **************")
