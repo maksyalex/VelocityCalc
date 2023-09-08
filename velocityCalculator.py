6705b8d75639f28380cc0727b88ea493b3a6819e
Alex <maksyalex@gmail.com>
2023-09-08 18:36:47 +1000

    secon commit if with an if statement

--- velocityCalculator.py
+++ velocityCalculator.py
@@ -1,12 +1,21 @@
 #Behold!...... a basic velocity calculator
 #v = d/t
-import math 
+import math
+
+answer = str(input("press d for kilometers and e for meters: "))
+
+if answer == 'e':
 # d is distance... 
-d = float(input("Enter distance you are going / have gone just numbers as a whole or decimal in KM: "))
+    d = float(input("Enter distance you are going / have gone just numbers as a whole or decimal in KM: "))
+
 # t is time...
-t = float(input("How many minutes did it take you: "))
+    t = float(input("How many minutes did it take you: "))
+
 # v is velocity 
-v = d/t 
-print('Your velocity is ', v, "per minute")
+    v = d/t 
+
+    print('Your velocity is ', v, "per minute")
+else:
+
 # there is much more to it, this is just extremely basic calculation based on some very traditional physics concepts. 
-print("********** MORE TO BE ADDED **************")
+    print("********** MORE TO BE ADDED **************")
